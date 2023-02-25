b8883364c103bbd5199d5f42096f986608530497
FantasyPvP <harry.blaise.irving@gmail.com>
2023-02-25 00:57:05 +0000

    .
    .

--- src/lib.rs
+++ src/lib.rs
@@ -10,3 +10,11 @@ mod kernel;
 pub mod std;
 
 extern crate alloc;
+
+
+pub fn init() {
+	kernel::gdt::init();
+	kernel::interrupts::init_idt();
+	unsafe { kernel::interrupts::PICS.lock().initialize() };
+	x86_64::instructions::interrupts::enable();
+}
