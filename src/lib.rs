7582260f2f48c56c370ed22f925c4fee9909eef8
FantasyPvP <harry.blaise.irving@gmail.com>
2023-02-25 01:10:47 +0000

    .
    .

--- src/lib.rs
+++ src/lib.rs
@@ -10,11 +10,21 @@ mod kernel;
 pub mod std;
 
 extern crate alloc;
+use bootloader::{BootInfo, entry_point};
 
-
-pub fn init() {
+pub fn init(boot_info: &'static BootInfo) {
 	kernel::gdt::init();
 	kernel::interrupts::init_idt();
 	unsafe { kernel::interrupts::PICS.lock().initialize() };
 	x86_64::instructions::interrupts::enable();
+
+    use x86_64::{structures::paging::{Page, Translate}, VirtAddr};
+
+	let physical_memory_offset = VirtAddr::new(boot_info.physical_memory_offset);
+	let mut mapper = unsafe { kernel::memory::init(physical_memory_offset) };
+	let mut frame_allocator = unsafe {
+		kernel::memory::BootInfoFrameAllocator::init(&boot_info.memory_map)
+	};
+
+    kernel::allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialisation failed");
 }
--- src/std/application.rs
+++ src/std/application.rs
@@ -16,3 +16,4 @@ pub enum Error {
 	CommandFailed(String),
 	EmptyCommand,
 }
+
